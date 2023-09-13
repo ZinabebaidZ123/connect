210e7d3c2fc3ff512094017237f7826d13cc03fa
ZinabebaidZ123 <60302323+ZinabebaidZ123@users.noreply.github.com>
2023-09-13 11:15:50 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+
+const wrapper = document.querySelector('.parteners')
+
+let pressed = false
+let startX = 0
+
+wrapper.addEventListener('mousedown', function (e) {
+  pressed = true
+  startX = e.clientX
+  this.style.cursor = 'grabbing'
+})
+
+wrapper.addEventListener('mouseleave', function (e) {
+  pressed = false
+})
+
+window.addEventListener('mouseup', function (e) {
+  pressed = false
+  wrapper.style.cursor = 'grab'
+})
+
+wrapper.addEventListener('mousemove', function (e) {
+  if(!pressed) {
+    return
+  }
+
+  this.scrollLeft += startX - e.clientX
+})
+
+window.addEventListener("load", function () {
+  // Hide the loader when the page finishes loading
+  document.getElementById("loader").style.display = "none";
+});
+
